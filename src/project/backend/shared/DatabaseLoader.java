cc84ef2dd02da4079b36bbe07c43865109bc7773
Michal Mazak <mazak.miso@gmail.com>
2018-04-26 12:11:07 +0200

    fixed database rebuild on item count mismatch

--- src/project/backend/shared/DatabaseLoader.java
+++ src/project/backend/shared/DatabaseLoader.java
@@ -21,18 +21,29 @@ public class DatabaseLoader extends Thread {
 
     @Override
     public void run() {
+        Platform.runLater(() -> Frontend.getTopPane().getInfoLabel().setText("Loading..."));
         File imageCacheDirectory = new File(Backend.DIRECTORY_PATH + "/imagecache/");
         if (!imageCacheDirectory.exists()) imageCacheDirectory.mkdir();
-        File databaseCacheFile = new File(Backend.DIRECTORY_PATH + "/database");
         ArrayList<DatabaseItem> databaseCache = Database.readFromDisk();
-        if (databaseCache == null || !databaseCacheFile.exists() || databaseCache.size() != Database.getFileCount()) {
-            rebuildCache();
-        } else {
-            Platform.runLater(() -> Frontend.getTopPane().getInfoLabel().setText("Loading..."));
+
+        if (databaseCache == null) rebuildCache();
+        else {
             loaderItemDatabase.addAll(databaseCache);
             for (DatabaseItem databaseItem : loaderItemDatabase) {
                 databaseItem.setImageView(new ImageView(getItemImage(databaseItem)));
                 databaseItem.setColoredText(new ColoredText(databaseItem.getSimpleName(), Color.BLACK, databaseItem));
+            }
+            if (databaseCache.size() != Database.getFileCount()) {
+                System.out.println("databaseCacheSize doesn't match validFileCount, building missing databaseItems...");
+                ArrayList<String> databaseCacheItemNames = new ArrayList<>();
+                for (DatabaseItem databaseItem : databaseCache)
+                    databaseCacheItemNames.add(databaseItem.getSimpleName());
+                for (File file : Database.getValidFiles())
+                    if (!databaseCacheItemNames.contains(file.getName())) {
+                        DatabaseItem newDatabaseItem = buildNewDatabaseItem(file);
+                        loaderItemDatabase.add(newDatabaseItem);
+                    }
+                Database.writeToDisk(loaderItemDatabase);
             }
         }
         Database.getItemDatabase().addAll(loaderItemDatabase);
@@ -48,16 +59,20 @@ public class DatabaseLoader extends Thread {
         Platform.runLater(() -> Frontend.getTopPane().getInfoLabel().setText("Loading done in " + Long.toString(System.currentTimeMillis() - loadingStartTimeMillis) + " ms"));
     }
 
+    private DatabaseItem buildNewDatabaseItem(File file) {
+        DatabaseItem newDatabaseItem = new DatabaseItem();
+        newDatabaseItem.setFullPath(file.getAbsolutePath());
+        newDatabaseItem.setSimpleName(file.getName());
+        newDatabaseItem.setExtension(FilenameUtils.getExtension(file.getName()));
+        newDatabaseItem.setImageView(new ImageView(getItemImage(newDatabaseItem)));
+        newDatabaseItem.setColoredText(new ColoredText(newDatabaseItem.getSimpleName(), Color.BLACK, newDatabaseItem));
+        newDatabaseItem.setTags(new ArrayList<>());
+        return newDatabaseItem;
+    }
+
     private void rebuildCache() {
         for (File file : Database.getValidFiles()) {
-            if (file.isDirectory()) continue;
-            DatabaseItem newDatabaseItem = new DatabaseItem();
-            newDatabaseItem.setFullPath(file.getAbsolutePath());
-            newDatabaseItem.setSimpleName(file.getName());
-            newDatabaseItem.setExtension(FilenameUtils.getExtension(file.getName()));
-            newDatabaseItem.setImageView(new ImageView(getItemImage(newDatabaseItem)));
-            newDatabaseItem.setColoredText(new ColoredText(newDatabaseItem.getSimpleName(), Color.BLACK, newDatabaseItem));
-            newDatabaseItem.setTags(new ArrayList<>());
+            DatabaseItem newDatabaseItem = buildNewDatabaseItem(file);
             loaderItemDatabase.add(newDatabaseItem);
             Platform.runLater(() -> Frontend.getTopPane().getInfoLabel().setText("Loading item " + (loaderItemDatabase.size() + 1) + " of " + Database.getFileCount() + ", " + (loaderItemDatabase.size() + 1) * 100 / Database.getFileCount() + "% done"));
         }
