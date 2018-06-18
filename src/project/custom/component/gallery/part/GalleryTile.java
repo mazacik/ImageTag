d5f23c091409a6c5b74e5ca1840bf854b4c74b19
Michal Mazak <mazak.miso@gmail.com>
2018-06-18 17:56:44 +0200

    galleryTile generateEffect fixed

--- src/project/custom/component/gallery/part/GalleryTile.java
+++ src/project/custom/component/gallery/part/GalleryTile.java
@@ -16,8 +16,10 @@ import project.database.DatabaseItem;
 public class GalleryTile extends ImageView {
     /* variables */
     private static InnerShadow selectionBorder = buildSelectionBorderEffect();
+    private static ColorInput focusMark = buildSelectionFocusMarkEffect();
+
     /* imports */
-    private final int galleryIconSizePref = Settings.getGalleryIconSizePref();
+    private static final int galleryIconSizePref = Settings.getGalleryIconSizePref();
 
     /* constructors */
     public GalleryTile(DatabaseItem databaseItem) {
@@ -26,6 +28,31 @@ public class GalleryTile extends ImageView {
         setFitHeight(galleryIconSizePref);
         setOnMouseClick(databaseItem);
     }
+
+    /* public methods */
+    public static void generateEffect(DatabaseItem databaseItem) {
+        boolean selection = Database.getDatabaseItemsSelected().contains(databaseItem);
+        boolean focus = false;
+        if (GalleryPaneFront.getInstance().getCurrentFocusedItem() != null)
+            focus = GalleryPaneFront.getInstance().getCurrentFocusedItem().equals(databaseItem);
+
+        if (!selection && !focus) {
+            databaseItem.getGalleryTile().setEffect(null);
+        } else if (!selection && focus) {
+            Blend blend = new Blend();
+            blend.setTopInput(focusMark);
+            databaseItem.getGalleryTile().setEffect(blend);
+        } else if (selection && !focus) {
+            databaseItem.getGalleryTile().setEffect(selectionBorder);
+        } else if (selection && focus) {
+            Blend effect = new Blend();
+            effect.setTopInput(selectionBorder);
+            effect.setBottomInput(focusMark);
+            effect.setMode(BlendMode.OVERLAY);
+            databaseItem.getGalleryTile().setEffect(effect);
+        }
+    }
+
     /* event methods */
     private void setOnMouseClick(DatabaseItem databaseItem) {
         setOnMouseClicked(event -> {
@@ -35,7 +62,8 @@ public class GalleryTile extends ImageView {
             }
         });
     }
-    /* private methods */
+
+    /* builder methods */
     private static InnerShadow buildSelectionBorderEffect() {
         InnerShadow innerShadow = new InnerShadow();
         innerShadow.setColor(Color.RED);
@@ -46,35 +74,11 @@ public class GalleryTile extends ImageView {
         innerShadow.setChoke(1);
         return innerShadow;
     }
-    //todo: fix me
-    /* public methods */
-    public void generateEffect(DatabaseItem databaseItem) {
-        boolean selection = Database.getDatabaseItemsSelected().contains(databaseItem);
-        boolean focus;
-        if (GalleryPaneFront.getInstance().getCurrentFocusedItem() == null) {
-            focus = false;
-        } else {
-            focus = GalleryPaneFront.getInstance().getCurrentFocusedItem().equals(databaseItem);
-        }
 
-        if (!selection && !focus) {
-            databaseItem.getGalleryTile().setEffect(null);
-        } else if (!selection && focus) {
-            int markSize = 6;
-            int markPosition = (galleryIconSizePref - markSize) / 2;
-            Blend effect = new Blend();
-            effect.setTopInput(new ColorInput(markPosition, markPosition, markSize, markSize, Color.RED));
-            databaseItem.getGalleryTile().setEffect(effect);
-        } else if (selection && !focus) {
-            databaseItem.getGalleryTile().setEffect(selectionBorder);
-        } else if (selection && focus) {
-            int markSize = 6;
-            int markPosition = (galleryIconSizePref - markSize) / 2;
-            Blend effect = new Blend();
-            effect.setTopInput(selectionBorder);
-            effect.setBottomInput(new ColorInput(markPosition, markPosition, markSize, markSize, Color.RED));
-            effect.setMode(BlendMode.OVERLAY);
-            databaseItem.getGalleryTile().setEffect(effect);
-        }
+    private static ColorInput buildSelectionFocusMarkEffect() {
+        int markSize = 6;
+        int markPositionInTile = (Settings.getGalleryIconSizePref() - markSize) / 2;
+        Color markColor = Color.RED;
+        return new ColorInput(markPositionInTile, markPositionInTile, markSize, markSize, markColor);
     }
 }
