bb94cf540aeb679d11247007555e6aa8bbca4f76
Michal <mazak.miso@gmail.com>
2020-02-12 22:32:59 +0100

    rework Settings

--- src/main/java/misc/Settings.java
+++ src/main/java/misc/Settings.java
@@ -5,59 +5,74 @@ import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
 
 public class Settings {
-	private static Integer COLOR_PRESET_INDEX;
-	private static Integer FONT_SIZE;
-	private static Integer GALLERY_TILE_SIZE;
-	
-	private transient static final Type typeToken;
-	static {
-		typeToken = new TypeToken<Settings>() {}.getType();
-		
-		try {
-			Settings settings = (Settings) JsonUtil.read(typeToken, FileUtil.getFileSettings());
-			COLOR_PRESET_INDEX = settings.colorPreset;
-			FONT_SIZE = settings.fontSize;
-			GALLERY_TILE_SIZE = settings.tileSize;
-		} catch (Exception e) {
-			COLOR_PRESET_INDEX = 0;
-			FONT_SIZE = 16;
-			GALLERY_TILE_SIZE = 200;
-		}
-	}
-	
 	private Integer colorPreset;
-	private Integer fontSize;
-	private Integer tileSize;
-	
-	private Settings() {
-	
-	}
-	
-	public static void writeToDisk() {
-		Settings settings = new Settings();
-		settings.colorPreset = COLOR_PRESET_INDEX;
-		settings.fontSize = FONT_SIZE;
-		settings.tileSize = GALLERY_TILE_SIZE;
-		JsonUtil.write(settings, typeToken, FileUtil.getFileSettings());
-	}
-	
+	private static final Integer colorPresetDefault = 0;
 	public static int getColorPreset() {
-		return COLOR_PRESET_INDEX;
+		return getInstance().colorPreset;
 	}
-	public static int getFontSize() {
-		return FONT_SIZE;
-	}
-	public static int getTileSize() {
-		return GALLERY_TILE_SIZE;
+	public static void setColorPreset(int colorPreset) {
+		getInstance().colorPreset = colorPreset;
 	}
 	
-	public static void setColorPreset(int colorPreset) {
-		Settings.COLOR_PRESET_INDEX = colorPreset;
+	private Integer fontSize;
+	private static final Integer fontSizeDefault = 16;
+	public static int getFontSize() {
+		return getInstance().fontSize;
 	}
 	public static void setFontSize(int fontSize) {
-		Settings.FONT_SIZE = fontSize;
+		getInstance().fontSize = fontSize;
+	}
+	
+	private Integer tileSize;
+	private static final Integer tileSizeDefault = 200;
+	public static int getTileSize() {
+		return getInstance().tileSize;
 	}
 	public static void setTileSize(int tileSize) {
-		Settings.GALLERY_TILE_SIZE = tileSize;
+		getInstance().tileSize = tileSize;
+	}
+	
+	private Integer collageSize;
+	private static final Integer collageSizeDefault = 25;
+	public static int getCollageSize() {
+		return getInstance().collageSize;
+	}
+	public static void setCollageSize(Integer collageSize) {
+		getInstance().collageSize = collageSize;
+	}
+	
+	private transient static final Type typeToken = new TypeToken<Settings>() {}.getType();
+	public static void writeToDisk() {
+		JsonUtil.write(getInstance(), typeToken, FileUtil.getFileSettings());
+	}
+	private static void readFromDisk() {
+		Settings settings;
+		try {
+			settings = (Settings) JsonUtil.read(typeToken, FileUtil.getFileSettings());
+		} catch (Exception e) {
+			settings = new Settings();
+		}
+		
+		if (settings.colorPreset == null) settings.colorPreset = colorPresetDefault;
+		if (settings.fontSize == null) settings.fontSize = fontSizeDefault;
+		if (settings.tileSize == null) settings.tileSize = tileSizeDefault;
+		if (settings.collageSize == null) settings.collageSize = collageSizeDefault;
+		
+		getInstance().colorPreset = settings.colorPreset;
+		getInstance().fontSize = settings.fontSize;
+		getInstance().tileSize = settings.tileSize;
+		getInstance().collageSize = settings.collageSize;
+	}
+	
+	static {
+		readFromDisk();
+	}
+	
+	private Settings() {}
+	private static class Loader {
+		private static final Settings INSTANCE = new Settings();
+	}
+	public static Settings getInstance() {
+		return Loader.INSTANCE;
 	}
 }
